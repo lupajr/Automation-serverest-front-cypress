9cdea1551b3b6635f888b977ac7f8970f8be0a54
Luiz Paulo Junior <thecanalgamebr@hotmail.com>
2025-06-20 23:40:51 -0300

    feat: adicionando xpath e cenário de erro login

--- cypress/e2e/login/spec.cy.js
+++ cypress/e2e/login/spec.cy.js
@@ -1,5 +1,16 @@
+require('@cypress/xpath');
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+  })
+
+
+  it('teste erro ao realizar login', () => {
+    cy.visit('front.serverest.dev')
+    cy.xpath('//input[@id="email"]').type('Email@fake.com')
+    cy.xpath('//input[@id="password"]').type('Mudar123')
+    cy.xpath('//button[@type="submit"]').click()
+    cy.xpath('//*[@role="alert"]').should('contain.text', 'Email e/ou senha inválidos')
   })
 })
